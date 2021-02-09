4ff188afc4435f12f4f59555150c62c456afe9d5
Ky13 <sky_io@outlook.com>
2021-02-09 13:42:53 +0800

    add updateURL & downloadURL

---
+++ html5/speed-control.stub.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        html5 video speed controller (vlc like)
+// @namespace   github.com/sky-bro
+// @version     0
+// @description Simple html5 video speed control with '-', '+', and '=' (vlc like). '-': decrease by 0.1, '+': increase by 0.1, '=': back to 1.0x
+// @author      https://sky-bro.github.io
+// @updateURL   https://raw.githubusercontent.com/sky-bro/tampermonkey-scripts/main/html5/speed-control.meta.js
+// @downloadURL   https://raw.githubusercontent.com/sky-bro/tampermonkey-scripts/main/html5/speed-control.user.js
+// ==/UserScript==
