c31e9ebd0d236eda1693db08fca43e24f05e9af6
Ky13 <sky_io@outlook.com>
2021-02-09 13:18:05 +0800

    change to +-=, cause {,} are taken by bilibili

--- html5/speed-control.js
+++ html5/speed-control.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name        html5 video speed controller (vlc like)
 // @namespace   github.com/sky-bro
-// @version     1.0.0
-// @description Simple html5 video speed control with '{', '}', and '=' (vlc like). '{': decrease by 0.1, '}': increase by 0.1, '=': back to 1.0x
+// @version     1.0.1
+// @description Simple html5 video speed control with '-', '+', and '=' (vlc like). '-': decrease by 0.1, '+': increase by 0.1, '=': back to 1.0x
 // @author      https://sky-bro.github.io
 // @match       https://www.youtube.com/*
+// @match       https://www.bilibili.com/*
 // @match       *://*.zhihuishu.com/*
 // @grant       none
 // @updateURL   https://github.com/sky-bro/tampermonkey-scripts/blob/main/html5/speed-control.js
@@ -12,23 +13,27 @@
 
 (function() {
     'use strict';
+    function setPlaybackRate(player, rate) {
+        if (rate < 0.1) rate = 0.1;
+        else if (rate > 10) rate = 10;
+        player.playbackRate = rate;
+        console.log("playing in %sx", (rate).toFixed(1));
+    }
+
     window.addEventListener('keypress', function(event) {
         var player = document.querySelector("video");
-        console.log(event);
+        // console.log(event);
         var curRate = Number(player.playbackRate);
         // vlc actually uses '[' and ']', but they are used by vimium
-        if (event.key == "{") {
-            console.log("{ pressed");
-            curRate -= 0.1;
-            player.playbackRate = (curRate < 0.1) ? 0.1 : curRate;
-        } else if (event.key == "}") {
-            console.log("} pressed");
-            curRate += 0.1;
-            player.playbackRate = (curRate > 10) ? 10 : curRate;
-        } else if (event.key == "=") {
+        if (event.key == "-") {
+            console.log("- pressed");
+            setPlaybackRate(player, curRate - 0.1);
+        } else if(event.key == "+") {
+            console.log("+ pressed");
+            setPlaybackRate(player, curRate + 0.1);
+        } else if(event.key == "=") {
             console.log("= pressed");
-            player.playbackRate = 1;
+            setPlaybackRate(player, 1);
         }
-        console.log("playing in %sx", (player.playbackRate).toFixed(1));
     });
 })();
